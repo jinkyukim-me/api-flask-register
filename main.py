062886565b5f227426d728ac785822a1edb40b55
jinkyu.kim <jinkyukim.dev@gmail.com>
2019-11-27 17:14:34 +0900

    Modify main.py

--- main.py
+++ main.py
@@ -16,7 +16,22 @@ mysql = MySQL(app)
 
 @app.route('/pythonlogin', methods=['GET','POST'])
 def login():
-	return render_template('index.html', msg='')
+	msg = ''
+	if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+		username = request.form['username']
+		password = request.form['password']
+		cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+		cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password))
+		account = cursor.fetchone()
+		if account:
+			session['loggedin'] = True
+			session['id'] = account['id']
+			session['username'] = account['username']
+			return '성공적으로 로그인 되셨습니다'
+		else:
+			msg = 'Incorrect username/password!'
+	
+	return render_template('index.html', msg=msg)
 
 if __name__ == '__main__':
 	app.run(debug=True)
