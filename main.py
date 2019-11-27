c286383d593474db99a52933c38230660f6fec8a
jinkyu.kim <jinkyukim.dev@gmail.com>
2019-11-27 22:11:40 +0900

    Update files

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def login():
 		username = request.form['username']
 		password = request.form['password']
 		cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-		cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password))
+		cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', [username, password])
 		account = cursor.fetchone()
 		if account:
 			session['loggedin'] = True
@@ -29,9 +29,42 @@ def login():
 			session['username'] = account['username']
 			return '성공적으로 로그인 되셨습니다'
 		else:
-			msg = 'Incorrect username/password!'
+			msg = '비밀번호가 다릅니다. 다시 확인해 주세요.'
 	
 	return render_template('index.html', msg=msg)
+
+@app.route('/pythonlogin/logout')
+def logout():
+	session.pop('loggedin', None)
+	session.pop('id', None)
+	session.pop('username', None)
+	return redirect(url_for('login'))
+
+@app.route('/pythonlogin/register', methods=['GET','POST'])
+def register():
+	msg=''
+	if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+		username = request.form['username']
+		password = request.form['password']
+		email = request.form['email']
+		cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+		cursor.execute('SELECT * FROM accounts WHERE username = %s', [username])
+		account = cursor.fetchone()
+		if account:
+			msg = '아이디가 중복이 됩니다. 다시 입력해 주세요.'
+		elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+			msg = '유효하지 않는 이메일 주소입니다.'
+		elif not re.match(r'[A-Za-z0-9]+', username):
+			msg = '한글이나 특수문자는 입력가능 합니다.영문과 숫자만 가능합니다.'
+		elif not username or not password or not email:
+			msg = '회원가입란에 작성을 해주세요'
+		else:
+			cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', [username, password, email])
+			mysql.connection.commit()
+			msg = '성공적으로 가입이 완료되었습니다.'
+	elif request.method == 'POST':
+		msg = '회원가입란에 작성을 해주세요'
+	return render_template('register.html', msg=msg)
 
 if __name__ == '__main__':
 	app.run(debug=True)
